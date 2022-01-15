f9902af82918a33618313469a0d7c8e51ec06f7a
heejun park <heejunpark@heejunui-MacBookPro.local>
2022-01-15 20:29:42 +0900

    Movie Information

--- src/App.js
+++ src/App.js
@@ -3,18 +3,41 @@ import { useState, useEffect } from "react";
 function App() {
   const [loding, setLoding] = useState(true);
   const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoding(false);
+  };
   useEffect(() => {
-    fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-    )
-      .then((response) => response.json())
-      .then((json) => {
-        setMovies(json.data.movies);
-        setLoding(false);
-      });
+    getMovies();
   }, []);
   console.log(movies);
-  return <div>{loding ? <h1>Loding...</h1> : null}</div>;
+  return (
+    <div>
+      {loding ? (
+        <h1>Loding...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
