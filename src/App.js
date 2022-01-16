b0935f281ccbe001f2382be94156820fec405ac5
heejun park <heejunpark@heejunui-MacBookPro.local>
2022-01-16 17:45:29 +0900

    Movie app2

--- src/App.js
+++ src/App.js
@@ -1,41 +1,5 @@
-import { useState, useEffect } from "react";
-import Movie from "./Movie";
-
 function App() {
-  const [loding, setLoding] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoding(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  console.log(movies);
-  return (
-    <div>
-      {loding ? (
-        <h1>Loding...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <Movie
-              key={movie.id}
-              coverImg={movie.medium_cover_image}
-              title={movie.title}
-              summary={movie.summary}
-              genres={movie.genres}
-            />
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return null;
 }
 
 export default App;
